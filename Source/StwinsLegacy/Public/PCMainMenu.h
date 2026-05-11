5ad7073aa6c2beb6e7e8d1f08db2236d2aedb100
0shiros <oli2014@gmx.fr>
2026-05-11 12:19:51 +0200

    Initial commit

---
+++ Source/StwinsLegacy/Public/PCMainMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PCMainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STWINSLEGACY_API APCMainMenu : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
